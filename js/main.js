032d494785558c6a751480466224a38819f63703
Alex7i7o <alexxaguero@gmail.com>
2026-04-21 00:27:38 -0300

    js para botones

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Imports
 import "../sass/Estilo.scss";
 
+// 
 document.addEventListener("DOMContentLoaded", () => {
     // Boton video
     const video = document.querySelector(".video");
@@ -24,5 +25,23 @@ document.addEventListener("DOMContentLoaded", () => {
                 playPauseBtn.textContent = "▶️";
             }
         });
+    }
+});
+
+// -------------
+
+const controls = document.querySelector('.video-controls');
+const introSection = document.querySelector('.intro'); // Tu contenedor padre
+
+window.addEventListener('scroll', () => {
+    // Calculamos cuánto le falta al contenedor para salir de pantalla
+    const rect = introSection.getBoundingClientRect();
+    const bottomThreshold = 150; // Ajusta esto: es a cuántos px del fondo quieres que cambie
+
+    // Si el borde inferior del contenedor está cerca del borde inferior de la pantalla
+    if (rect.bottom < bottomThreshold) {
+        controls.classList.add('is-absolute');
+    } else {
+        controls.classList.remove('is-absolute');
     }
 });
