16538dc81bbaa66ec0e31f7e7495895d3c5f6ebc
Alex7i7o <alexxaguero@gmail.com>
2026-04-20 23:24:07 -0300

    Video fondo preuba

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 // Imports
 import "../sass/Estilo.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Boton video
+    const video = document.querySelector(".video");
+    const audioBtn = document.getElementById("audioBtn");
+    const playPauseBtn = document.getElementById("playPauseBtn");
+
+    if(video && audioBtn && playPauseBtn) {
+        // Control de Audio
+        audioBtn.addEventListener("click", () => {
+            video.muted = !video.muted;
+            audioBtn.textContent = video.muted ? "🔊" : "🔇";
+        });
+
+        // Control de Play/Pausa
+        playPauseBtn.addEventListener("click", () => {
+            if (video.paused) {
+                video.play();
+                playPauseBtn.textContent = "⏸️";
+            } else {
+                video.pause();
+                playPauseBtn.textContent = "▶️";
+            }
+        });
+    }
+});
